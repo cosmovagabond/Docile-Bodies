e4548736c2f9030ef9a72d0d8bfbec32e38f5872
cosmovagabond <cosmovagabond@gmail.com>
2017-06-05 14:26:18 -0700

    Revert "20161217 Documenting"
    This reverts commit e461c01f10e1f575a93b275e5ae0a666feb9c9f0.
    
    # Conflicts:
    #	C#ScriptsDocuments.docx
    #
    Gesture_and_Zone_Detection_Test/.vs/Gesture_and_Zone_Detection_Test/v14/
    .suo
    #	Gesture_and_Zone_Detection_Test/Library/CurrentLayout.dwlt
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    -Editor-firstpass.dll
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    -Editor-firstpass.dll.mdb
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    -Editor.dll
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    -Editor.dll.mdb
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    -firstpass.dll
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    -firstpass.dll.mdb
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    .dll
    #
    Gesture_and_Zone_Detection_Test/Library/ScriptAssemblies/Assembly-CSharp
    .dll.mdb
    #	Gesture_and_Zone_Detection_Test/Library/assetDatabase3
    #
    Gesture_and_Zone_Detection_Test/Library/metadata/00/00000000000000004000
    000000000000
    #
    Gesture_and_Zone_Detection_Test/Library/metadata/00/00000000000000006100
    000000000000
    #
    Gesture_and_Zone_Detection_Test/Library/shadercompiler-UnityShaderCompil
    er.exe0.log
    #	Summary.docx

--- Gesture_and_Zone_Detection_Test/Assets/Script/PhaseChange.cs
+++ Gesture_and_Zone_Detection_Test/Assets/Script/PhaseChange.cs
@@ -1,18 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-//This method PhaseChange gets data from KinectManager.cs and set up different phase values, which is used by other scripts.
 public class PhaseChange : MonoBehaviour {
 
-    //PhaseInput1 and PhaseInput2 are used to determine if the Kinect is getting data that switch phases
     public int PhaseInput1;
     public int PhaseInput2;
-    //Phase is the actual value that determine the phase
     public int Phase;
-    //Not used now
     public float test;
-    //timer1 and timer2 are used to make sure that phase changes under the condition that the data getting from Kinect are consistent for over a certain amount of time    
+
     private float timer1;
     private float timer2;
 
@@ -23,8 +18,7 @@ public class PhaseChange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate() {
-        
-        //Start the timer when getting data from Kinect
+
         if (PhaseInput1 == 1)
         {
             timer1 = timer1 + Time.deltaTime;
@@ -35,7 +29,6 @@ public class PhaseChange : MonoBehaviour {
             timer2 = timer2 + Time.deltaTime;
         }
 
-        //Dump the timer when no phase changing is happening
         if (PhaseInput1 == 0)
         {
             timer1 = 0;
@@ -46,7 +39,6 @@ public class PhaseChange : MonoBehaviour {
             timer2 = 0;
         }
 
-        //Trigger change on value of Phase if the Kinect streams consistent data over 1 second
         if (timer1 > 1)
         {
             Phase = 1;
--- Gesture_and_Zone_Detection_Test/Assets/Script/Transformation.cs
+++ Gesture_and_Zone_Detection_Test/Assets/Script/Transformation.cs
@@ -1,30 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-//The method Transformation extract data from PhaseChange.cs and gets data from KinectManager.cs, determine whether it's going to transform from male to female or female to male or stay the same, then stream result into DoubleSideShaderMorph.cs and DoubleSideShaderMorph1.cs
 public class Transformation : MonoBehaviour
 {
 
-    //MaleGesture and FemaleGesture are used to gather data from KinectManager.cs to determine whether the Kinect is recognizing male gesture or female gesture
     public bool MaleGesture;
     public bool FemaleGesture;
-    //Access the two scripts DoubleSideShaderMorph.cs and DoubleSideShaderMorph1.cs
-    //I do not remember why I defined two different variables for the same scripts, I am not sure whether if it's necessary or if it's actually doing something
     public DoubleSideShaderMorph DoubleSideShaderMorph_lower;
     public DoubleSideShaderMorph DoubleSideShaderMorph_upper;
     public DoubleSideShaderMorph1 DoubleSideShaderMorph1_lower;
     public DoubleSideShaderMorph1 DoubleSideShaderMorph1_upper;
-    //Access PhhaseChange.cs
     public PhaseChange PhaseChange;
-    //KeyCheck is used in DoubleSideShaderMorph.cs and DoubleSideShaderMorph1.cs to control model transformation between male version and female version
+
     private float KeyCheck;
-    //A set of timers are used to determine whether a pose is hold long enough to trigger the event. Thus important events won't be triggered by random poses
     public float timer1;
     public float dumptimer1;
     public float timer2;
     public float dumptimer2;
-    //Phase is the local variable that matches PhaseChange.cs's Phase value
     private int Phase;
 
     // Use this for initialization
@@ -36,31 +28,28 @@ public class Transformation : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Extract Phase value from PhaseChange.cs and apply it to local variable Phase
         Phase = PhaseChange.Phase;
-        //Phase 1 events
+
         if (Phase == 1)
         {
-            //If Kinect regonizes a pose as malegesture, timer1 starts and reset dumptimer1
             if (MaleGesture)
             {
                 timer1 = timer1 + Time.deltaTime;
                 dumptimer1 = 0;
             }
-            //If Kinect records a non-malegesture pose the dumptimer starts, this was create to prevent Kinect give random false value which result restarting timer1
+            //timer for how long the gesture must be held to trigger a "1"
             if (MaleGesture != true)
             {
                 dumptimer1 = dumptimer1 + Time.deltaTime;
             }
-            //If the the false value is streamed from Kinect for more than half seconds also it does not get dumped cause there is no male gesture being recorded then it's considered a "real" false value, when that happens reset timer1
+
             if (dumptimer1 > 0.5)
             {
                 timer1 = 0;
             }
-            //If the male gesture is held for more than 1 second, then proceed to next step
+            // if timer is more than 1, then check to see if model is in "male state", if no, then...
             if (timer1 > 1)
             {
-                //Checking if the model is already in "male state", if yes, do nothing. If no, access DoubleSideShaderMorph.cs and DoubleSideShaderMorph1.cs to change the KeyCheck value to 1 to initiate transformation from female state to male state
                 if ((DoubleSideShaderMorph_lower.blendshape < 100) & (DoubleSideShaderMorph_lower.blendshape > 0) & (DoubleSideShaderMorph_upper.blendshape < 100) & (DoubleSideShaderMorph_upper.blendshape > 0))
                 {
                     return;
@@ -75,26 +64,25 @@ public class Transformation : MonoBehaviour
                 }
             }
 
-            //If Kinect regonizes a pose as malegesture, timer2 starts and reset dumptimer2
             if (FemaleGesture)
             {
                 timer2 = timer2 + Time.deltaTime;
                 dumptimer2 = 0;
             }
-            //If Kinect records a non-malegesture pose the dumptimer starts, this was create to prevent Kinect give random false value which result restarting timer2
+
             if (FemaleGesture != true)
             {
                 dumptimer2 = dumptimer2 + Time.deltaTime;
             }
-            //If the the false value is streamed from Kinect for more than half seconds also it does not get dumped cause there is no male gesture being recorded then it's considered a "real" false value, when that happens reset timer2
+
             if (dumptimer2 > 0.5)
             {
                 timer2 = 0;
             }
-            //If the female gesture is held for more than 1 second, then proceed to next step
+
+            // if timer is more than 1, then check to see if model is in "female state", if no, then...
             if (timer2 > 1)
             {
-                //Checking if the model is already in "female state", if yes, do nothing. If no, access DoubleSideShaderMorph.cs and DoubleSideShaderMorph1.cs to change the KeyCheck value to 1 to initiate transformation from male state to female state
                 if ((DoubleSideShaderMorph_lower.blendshape < 100) & (DoubleSideShaderMorph_lower.blendshape > 0) & (DoubleSideShaderMorph_upper.blendshape < 100) & (DoubleSideShaderMorph_upper.blendshape > 0))
                 {
                     return;
@@ -109,7 +97,7 @@ public class Transformation : MonoBehaviour
             }
         }
 
-        //Phase 2 events, these are for testing purpose only now, no pratical functions yet.
+
         if (Phase == 2)
         {
             DoubleSideShaderMorph1_lower.KeyCheck = 1;
